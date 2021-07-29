1ff5d1431986c79f9d42503a820b1fce62691b2d
Mourad Aouinat <mourad.aouinat@4d.com>
2021-07-29 07:25:01 +0100

    chore: remove unused vars

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,7 +4,7 @@ import { useTodos } from "../Provider";
 import { ITodo } from "../interfaces";
 
 const TodoItem: React.FC<{ todo: ITodo }> = ({ todo }) => {
-  const { todos, setTodos } = useTodos();
+  const { setTodos } = useTodos();
   const [isEditing, setIsEditing] = useState(false);
   const [inputValue, setInputValue] = useState(todo.title);
 
